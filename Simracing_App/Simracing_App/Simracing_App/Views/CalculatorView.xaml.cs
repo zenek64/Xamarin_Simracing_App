708556baf937231fa3e1b9edd7aa3d3eb4ef411c
Grzegorz <46374229+zenek64@users.noreply.github.com>
2023-04-11 20:30:32 +0200

    Adding Main Menu

---
+++ Simracing_App/Simracing_App/Simracing_App/Views/CalculatorView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Simracing_App.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CalculatorView : ContentPage
+	{
+        private int _time;
+        private int _min;
+        private int _sec;
+        private double _fuel;
+        public CalculatorView()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _time = int.Parse(race_length.Text.ToString());
+            _min = int.Parse(lap_time_min.Text.ToString());
+            _sec = int.Parse(lap_time_sec.Text.ToString());
+            _fuel = double.Parse(fuel_per_lap.Text.ToString());
+
+            double minimum;
+            double recommended;
+            double safe;
+
+            minimum = (((_time * 60) / ((_min * 60) + _sec)) * _fuel) + _fuel;
+            recommended = (((_time * 60) / ((_min * 60) + _sec)) * _fuel) + (_fuel * 2);
+            safe = (((_time * 60) / ((_min * 60) + _sec)) * _fuel) + (_fuel * 3);
+
+            result.Text = minimum.ToString();
+            result_rec.Text = recommended.ToString();
+            result_safe.Text = safe.ToString();
+        }  
+    }
+}
--- Simracing_App/Simracing_App/Simracing_App/Views/MainPage.xaml.cs
+++ Simracing_App/Simracing_App/Simracing_App/Views/MainPage.xaml.cs
@@ -11,37 +11,19 @@ namespace Simracing_App
 {
     public partial class MainPage : ContentPage
     {
-        private int time;
-        private int min;
-        private int sec;
-        private double fuel;
+        
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void Button_Clicked(object sender, EventArgs e)
+        private void CalculatorButtonClicked(object sender, EventArgs e)
         {
-            time = int.Parse(race_length.Text.ToString());
-            min = int.Parse(lap_time_min.Text.ToString());
-            sec = int.Parse(lap_time_sec.Text.ToString());
-            fuel = double.Parse(fuel_per_lap.Text.ToString());
-
-            double minimum;
-            double recommended;
-            double safe;
-
-            minimum = (((time * 60) / ((min * 60) + sec)) * fuel) + fuel;
-            recommended = (((time * 60) / ((min * 60) + sec)) * fuel) + (fuel * 2);
-            safe = (((time * 60) / ((min * 60) + sec)) * fuel) + (fuel * 3);
-
-            result.Text = minimum.ToString();
-            result_rec.Text = recommended.ToString();
-            result_safe.Text = safe.ToString();
+            Navigation.PushAsync(new CalculatorView());
         }
-        private void OnButtonClicked(object sender, EventArgs e)
+        private void TrackRecordsButtonClicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new TrackRecordView());
         }
+        
     }
 }
