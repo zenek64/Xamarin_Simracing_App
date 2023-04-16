a8371623cc44a29a59824c105b11e9caf3f40fc8
Grzegorz <46374229+zenek64@users.noreply.github.com>
2023-04-16 18:24:02 +0200

    Adding Time

--- Simracing_App/Simracing_App/Simracing_App/Models/TrackRecord.cs
+++ Simracing_App/Simracing_App/Simracing_App/Models/TrackRecord.cs
@@ -12,5 +12,10 @@ namespace Simracing_App.Models
         public int ID { get; set; }
         public string TrackName { get; set; }
         public string CarName { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        public int Milliseconds { get; set; }
+
+
     }
 }
