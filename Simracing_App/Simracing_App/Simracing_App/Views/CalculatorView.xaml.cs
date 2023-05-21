93c7d986f3b0dc1e435c0a858e22e01569133dc3
Grzegorz <46374229+zenek64@users.noreply.github.com>
2023-05-21 21:00:21 +0200

    Validation v1

--- Simracing_App/Simracing_App/Simracing_App/Views/CalculatorView.xaml.cs
+++ Simracing_App/Simracing_App/Simracing_App/Views/CalculatorView.xaml.cs
@@ -22,22 +22,57 @@ namespace Simracing_App.Views
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            _time = int.Parse(race_length.Text.ToString());
-            _min = int.Parse(lap_time_min.Text.ToString());
-            _sec = int.Parse(lap_time_sec.Text.ToString());
-            _fuel = double.Parse(fuel_per_lap.Text.ToString());
+            _time = SetTime(race_length.Text.ToString());
+            _min = SetTime(lap_time_min.Text.ToString());
+            _sec = SetTime(lap_time_sec.Text.ToString());
+            _fuel = SetFuel(fuel_per_lap.Text.ToString());
 
-            double minimum;
-            double recommended;
-            double safe;
-
-            minimum = (((_time * 60) / ((_min * 60) + _sec)) * _fuel) + _fuel;
-            recommended = minimum + _fuel;
-            safe = recommended + _fuel;
+            double minimum = CalculateFuel(_time, _min, _sec, _fuel);
+            double recommended = CalculateFuel(_time, _min, _sec, _fuel) + _fuel;
+            double safe = CalculateFuel(_time, _min, _sec, _fuel) + (2 * _fuel);
 
             result.Text = minimum.ToString();
             result_rec.Text = recommended.ToString();
             result_safe.Text = safe.ToString();
-        }  
+        }
+        
+        private double CalculateFuel(int time, int min, int sec, double fuel)
+        {
+            double reqFuel;
+
+            reqFuel = (((time * 60) / ((min * 60) + sec)) * fuel) + fuel;
+
+            return reqFuel;
+        }
+        private int SetTime(string n)
+        {
+            int number;
+
+            bool success = int.TryParse(n, out number);
+            if (success)
+            {
+                return number;
+            }
+            else
+            {
+                number = 1;
+                return number;
+            }
+        }
+        private double SetFuel(string n) 
+        {
+            double number;
+            
+            bool success = double.TryParse(n, out number);
+            if(success)
+            {
+                return number;
+            }
+            else
+            { 
+                number = 1; 
+                return number; 
+            }
+        }
     }
 }
